95aa37d6fc373555eeb379d90f99ceb904e681fa
alexanderStamenkov <sa6ko2000@abv.bg>
2026-06-12 00:24:44 +0300

    fixed .env and vite bug

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // Превръщаме пътя до frontend в абсолютен
   root: resolve(__dirname, "frontend"),
+  // ТОВА КАЗВА НА VITE ДА ТЪРСИ .ENV ФАЙЛОВЕТЕ В ГЛАВНАТА ПАПКА CHAT:
+  envDir: resolve(__dirname),
   build: {
-    // Казваме на Vite да създаде dist директно в корена на главната папка Chat
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        // Абсолютни пътища до всеки един от HTML файловете ти
         main: resolve(__dirname, "frontend/index.html"),
         auth: resolve(__dirname, "frontend/auth/auth.html"),
         chat: resolve(__dirname, "frontend/chat/chat.html"),
