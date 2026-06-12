a7efb87cf1c8f930f1948fef245f4917c9c0185c
alexanderStamenkov <sa6ko2000@abv.bg>
2026-06-12 21:11:46 +0300

    fix build config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  // loadEnv автоматично събира променливите от .env файловете И от системата на Netlify
-  const env = loadEnv(mode, process.cwd(), "");
-
-  console.log("=== NETLIFY ENV DEBUGGER ===");
-  console.log("Netlify URL:", env.VITE_SUPABASE_URL);
-  console.log("=============================");
-
-  return {
-    root: "frontend", // Тъй като index.html е вътре във frontend папка
-    envDir: "../", // Търси локалните .env файлове една папка нагоре (в CHAT)
-    build: {
-      outDir: "../dist", // Създава папка dist в главната директория CHAT
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          main: "index.html", // Търси се в frontend/index.html
-          auth: "auth/auth.html", // Търси се в frontend/auth/auth.html
-          chat: "chat/chat.html", // Търси се в frontend/chat/chat.html
-        },
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        auth: "auth/auth.html",
+        chat: "chat/chat.html",
       },
     },
-  };
+  },
 });
