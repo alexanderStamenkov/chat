4088129e91218f94980ccc1b989e2e5644466af0
alexanderStamenkov <sa6ko2000@abv.bg>
2026-06-12 21:23:12 +0300

    fix vite config absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  root: resolve(__dirname, "frontend"),
   build: {
-    outDir: "dist",
+    outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: "index.html",
-        auth: "auth/auth.html",
-        chat: "chat/chat.html",
+        main: resolve(__dirname, "frontend/index.html"),
+        auth: resolve(__dirname, "frontend/auth/auth.html"),
+        chat: resolve(__dirname, "frontend/chat/chat.html"),
       },
     },
   },
